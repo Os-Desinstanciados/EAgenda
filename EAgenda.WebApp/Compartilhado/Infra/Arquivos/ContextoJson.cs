32fec528afaca89a9e54f04fef191e2e75ec3b89
Alexandre Rocha <juny14_15@hotmail.com>
2026-06-25 13:48:20 -0300

    Adicionado Contexto.Json

---
+++ EAgenda.WebApp/Compartilhado/Infra/Arquivos/ContextoJson.cs
@@ -0,0 +1,62 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using EAgenda.WebApp.Modulos.ModuloCompromisso.Dominio;
+using EAgenda.WebApp.Modulos.ModuloContato.Dominio;
+
+
+namespace EAgenda.WebApp.Compartilhado.Infra.Arquivos;
+
+public sealed class ContextoJson
+{
+    private readonly string caminhoArquivo;
+
+    public List<Contato> Contatos { get; set; } = new List<Contato>();
+    public List<Compromisso> Compromissos { get; set; } = new List<Compromisso>();
+
+    public ContextoJson()
+    {
+        string caminhoAppData = Environment
+            .GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        string caminhoDiretorio = Path.Combine(caminhoAppData, "ControleDeMedicamentosWeb");
+
+        Directory.CreateDirectory(caminhoDiretorio);
+
+        caminhoArquivo = Path.Combine(caminhoDiretorio, "dados.json");
+    }
+
+    public void Salvar()
+    {
+        JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
+        opcoesJson.WriteIndented = true;
+        opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
+        opcoesJson.Converters.Add(new JsonStringEnumConverter());
+
+        string jsonString = JsonSerializer.Serialize(this, opcoesJson);
+
+        File.WriteAllText(caminhoArquivo, jsonString);
+    }
+
+    public void Carregar()
+    {
+        if (!File.Exists(caminhoArquivo))
+            return;
+
+        string jsonString = File.ReadAllText(caminhoArquivo);
+
+        JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
+        opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
+        opcoesJson.Converters.Add(new JsonStringEnumConverter());
+
+        ContextoJson? contextoSalvo = JsonSerializer
+            .Deserialize<ContextoJson>(jsonString, opcoesJson);
+
+        if (contextoSalvo == null)
+            return;
+
+        Contatos = contextoSalvo.Contatos ?? new List<Contato>();
+        Compromissos = contextoSalvo.Compromissos ?? new List<Compromisso>();
+    }
+}
