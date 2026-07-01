4df0086978225e71358026f2c28749b53f2db05a
Alexandre Rocha <juny14_15@hotmail.com>
2026-07-01 16:13:41 -0300

    Implementado as tabelas

---
+++ EAgenda.Web.Database/dbo/Tables/TBContato.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[TBContato] (
+    [Id]       UNIQUEIDENTIFIER NOT NULL,
+    [Nome]     NVARCHAR (100)   NOT NULL,
+    [Email]    NVARCHAR (100)   NOT NULL,
+    [Telefone] NVARCHAR (15)    NOT NULL,
+    [Cargo]    NVARCHAR (100)   NULL,
+    [Empresa]  NVARCHAR (100)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
